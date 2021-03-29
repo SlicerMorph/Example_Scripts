e48a925aa965ca2919bb81ff30b33deab6743081
SlicerMorph Project <SlicerMorph@outlook.com>
2021-03-28 22:39:02 -0700

    Update Extra_Markups_panel.py

--- Extra_Markups_panel.py
+++ Extra_Markups_panel.py
@@ -1,3 +1,5 @@
+# Copy and paste this code to show an extra panel of Markups module outside of the Slicer's main application window. 
+
 markups_module_widget = slicer.modules.markups.createNewWidgetRepresentation()
 markups_module_widget.setMRMLScene(slicer.mrmlScene)
 markups_module_widget.show()
